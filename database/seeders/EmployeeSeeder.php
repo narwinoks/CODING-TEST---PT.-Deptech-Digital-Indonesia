7f11b1448b0d4260122508273c028e19c9d93a78
narwinoks <narnowin195@gmail.com>
2023-07-09 16:37:59 +0700

    add seeder

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $model = new Employee();
+        $model->first_name = "john";
+        $model->last_name = "doe";
+        $model->email = "john@example.com";
+        $model->phone = "081477084165";
+        $model->gender = "laki laki";
+        $model->address = "solo";
+        $model->save();
     }
 }
